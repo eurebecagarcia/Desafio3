9b4739cc5739d04a8069bbdb31c0dbf64ac75360
eu.rebecagarcia <rebecagarciarodrigues21@gmail.com>
2022-06-13 20:30:44 -0300

    desafio 03

---
+++ script.js
@@ -0,0 +1,19 @@
+for(let number = 1; number <= 10; number++){
+    console.log(number)
+}
+
+
+let num = 10;
+while(num >= 1){
+    console.log(num);
+    num--
+}
+
+for(let num = 1; num <= 100; num +=2){
+    console.log(`Sempre impar ${num}`)
+}
+
+for(let number = 0; number <= 100; number +=2){
+    console.log(`Sempre par ${number}`)
+
+}
